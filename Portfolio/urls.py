1cc2e3d9d99e112ad3bf4eb29fc5d6912ee7d286
Nayan-E-Alam <nayanmasters@gmail.com>
2023-11-29 03:27:15 +0600

    url

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -1,8 +1,21 @@
 
+from django.contrib import admin
 from django.urls import path
-from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
+from website import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('ABOUT/', views.ABOUT),
+    path('CONTACT/', views.CONTACT),
+    path('GALLARY/', views.GALLARY),
     path('', views.HOME),
+    path('SERVICE/', views.SERVICE),
+    path('UPDATE/', views.UPDATE),
+ 
 ]
+
+if settings.DEBUG:
+    urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    
