129ddc4eb2cf75ca63ee4105679b1e401646bd6c
Nayan-E-Alam <nayanmasters@gmail.com>
2023-11-29 01:06:09 +0600

    About

--- Portfolio/views.py
+++ Portfolio/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-
-def HOME(request):
-    return render(request,"index.html")
-
+from service.models import Service
+def ABOUT(request):
+    data={
+         'title':'portfolio'
+    }
+    return render(request,"about.html",data)
