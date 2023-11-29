6e1eaf8756b85ba013b5c059cd867b355598175f
zarinsharmin <zarinshormi97@gmail.com>
2023-11-29 12:54:24 +0600

    modifying design

--- Portfolio/models.py
+++ Portfolio/models.py
@@ -1,12 +1,4 @@
 from django.db import models
 
 
-class Service(models.Model):
-    website_title = models.CharField(max_length=10)
-    website_description = models.TextField()
-    date = models.DateField()
-    image = models.FileField(upload_to="news/", max_length=250, null=True, default=None)
-    status = models.TextField()
-
-
 # Create your models here.
