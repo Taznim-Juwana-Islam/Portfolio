ecbdfe533355d839dbe23b138e502f16759a70a8
Taznim-Juwana-Islam <78605126+Taznim-Juwana-Islam@users.noreply.github.com>
2023-11-29 21:47:43 +0600

    view and url

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path("", views.HOME),
+    path('admin/', admin.site.urls),
+    path('ABOUT/', views.ABOUT),
+    path('CONTACT/', views.CONTACT),
+    path('GALLARY/', views.GALLARY),
+    path('', views.HOME),
+    path('SERVICE/', views.SERVICE),
+    path('UPDATE/', views.UPDATE),
+ 
 ]
+
+if settings.DEBUG:
+    urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    
--- Service/admin.py
+++ Service/admin.py
@@ -3,4 +3,4 @@ from service.models import Service
 class ServiceAdmin(admin.ModelAdmin):
     list_display=('website_title','website_description','date','status','image')
 admin.site.register(Service,ServiceAdmin)
-# Register your models here.
+
