f0e308bf90004d3638bab7b30c1b0f8aa29ddd8d
zarinsharmin <zarinshormi97@gmail.com>
2023-11-30 01:18:48 +0600

    update

--- Service/admin.py
+++ Service/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from service.models import Service
-class ServiceAdmin(admin.ModelAdmin):
-    list_display=('website_title','website_description','date','status','image')
-admin.site.register(Service,ServiceAdmin)
+from Service.models import Service
 
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("website_title", "website_description", "date", "status", "image")
+
+
+admin.site.register(Service, ServiceAdmin)
