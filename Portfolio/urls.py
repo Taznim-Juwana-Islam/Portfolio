9c9eded4c20f1d01a1f481f7cb78b7e698b701c9
Nayan-E-Alam <nayanmasters@gmail.com>
2023-11-30 12:48:31 +0600

    updated

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -9,14 +9,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('ABOUT/', views.ABOUT),
     path('CONTACT/', views.CONTACT),
     path('GALLARY/', views.GALLARY),
     path('', views.HOME),
     path('SERVICE/', views.SERVICE),
     path('UPDATE/', views.UPDATE),
- 
 ]
 
 if settings.DEBUG:
